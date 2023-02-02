7e6b105fda1faa55ebdf58b85345b9e4f10db313
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-02-02 22:53:28 +0100

    SOLUTION TASK 5

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -3,12 +3,12 @@ package lms_126
 fun task5(): String {
 
     return  """INPUT Monday is 1
-        Tuesday is 2 
-        Wednesday is 3 
-        Thursday is 4 
-        Friday is 5 
-        Saturday is 6 
-        Sunday is 7 
+        INPUT Tuesday is 2 
+        INPUT Wednesday is 3 
+        INPUT Thursday is 4 
+        INPUT Friday is 5 
+        INPUT Saturday is 6 
+        INPUT Sunday is 7 
         IF 1 = Monday 
         IF 2 = Tuesday 
         TF 3 = Wednesday 
