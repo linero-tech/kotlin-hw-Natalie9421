7fde0a40fd09274754586bf9cd1e5605656a9c25
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-02-02 22:41:33 +0100

    solution task 5

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,21 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return  """INPUT Monday is 1
+        Tuesday is 2 
+        Wednesday is 3 
+        Thursday is 4 
+        Friday is 5 
+        Saturday is 6 
+        Sunday is 7 
+        IF 1 = Monday 
+        IF 2 = Tuesday 
+        TF 3 = Wednesday 
+        IF 4 = Thursday 
+        IF 5 = Friday 
+        IF 6 = Saturday 
+        IF 7 = Sunday 
+        OUTPUT day of the week 
+        
+    """.trimMargin()
 }
