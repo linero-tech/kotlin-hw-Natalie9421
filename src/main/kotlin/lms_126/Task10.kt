0fa89a7ebc98070aab2bbe1dcb8ebd8a68c5c2fc
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-02-06 23:32:51 +0100

    solution task 10

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,26 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """ INPUT click create an account 
+         INPUT fill first name and last name 
+         INPUT fill username 
+         IF username is taken 
+         OUTPUT try another 
+         ELSE continue 
+         INPUT fill password more than six characters 
+         IF password is right 
+         OUTPUT continue 
+         ELSE try another password 
+         INPUT confirm password 
+         INPUT phone number 
+         INPUT verify your phone number with the verification code that you get as message text 
+         INPUT recovery email address 
+         INPUT birthday month, day , year 
+         INPUT gender 
+         INPUT accept the police 
+         IF not 
+         OUTPUT error 
+         ELSE 
+         OUTPUT new email account 
+    """.trimMargin()
 }
