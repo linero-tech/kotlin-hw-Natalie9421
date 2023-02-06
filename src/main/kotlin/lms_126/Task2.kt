eeb955861ef734b333aab6af8bb827f704f13185
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-02-06 10:58:13 +0100

    Update Task2.kt

--- src/main/kotlin/lms_126/Task2.kt
+++ src/main/kotlin/lms_126/Task2.kt
@@ -4,7 +4,7 @@ fun task2(): String {
 
     return """ INPUT base of rectangle
         INPUT height of rectangle
-        calculate area by multiplying base times hight 
+        calculate area by multiplying base times height 
         OUTPUT area of rectangle 
         
     """.trimMargin()
