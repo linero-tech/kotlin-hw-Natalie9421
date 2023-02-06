f25b9bd79cf5c5bce307c119e0f2548e52bb9793
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-02-06 16:44:46 +0100

    solution task 9

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,16 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return   """ INPUT Set in credit card in the ATM 
+        INPUT Write credit card code 
+        INPUT press okay to confirm 
+        INPUT choose the withdraw method 
+        INPUT choose the amount of money you want ex.200 kr. 
+        INPUT okay to confirm 
+        IF the withdraw > from your bank balance 
+        OUTPUT error 
+        ELSE get the money 
+        
+         
+    """.trimMargin()
 }
