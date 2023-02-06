b56685b4fdfae7db282da9e432ee413a40bca9a3
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-02-06 16:54:38 +0100

    solution task 11

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,10 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """INPUT the temperature
+        IF last letter is C 
+        covert to F
+        ELSE 
+        convert to C 
+    """.trimMargin()
 }
