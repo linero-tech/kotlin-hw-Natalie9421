a390c8aa1e06b7e70e3fa818a31d98add80e5057
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-02-06 12:18:38 +0100

    solution task 8

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -2,5 +2,24 @@ package lms_126
 
 fun task8(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """INPUT X is number 
+        INPUT Foo = x * 3 
+        INPUT Bar = x * 5 
+        INPUT FooBar = x * 3 * 5 
+        INPUT Qix =  x not * 3 or x not * 5 
+        
+        
+        IF x = x * 3 
+        OUTPUT Foo 
+        
+        IF x = x * 5
+        OUTPUT Bar 
+         
+        IF x = x * 3 * 5
+        OUTPUT FooBar 
+        
+        IF x = not x * 3 or x = not x * 5 
+        OUTPUT Qix 
+        
+    """.trimMargin()
 }
