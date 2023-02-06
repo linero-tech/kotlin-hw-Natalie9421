758f1f2c145adeede8e6f70e83e273dfd7d5ea0e
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-02-06 13:26:00 +0100

    solution task 6

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,5 +2,10 @@ package lms_126
 
 fun task6(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return  """INPUT x all numbers between  1 to 100 
+        INPUT x = x/5 = 0 
+        
+         OUTPUT X  
+         
+    """.trimMargin()
 }
