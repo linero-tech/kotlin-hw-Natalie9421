e433801008760d15ade65ac0f195d3326e24f53a
natalie khoury <123169299+Natalie9421@users.noreply.github.com>
2023-03-07 14:07:48 +0100

    task 9 103

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -1,6 +1,16 @@
 package lms_129
 
-fun task9(temperature: String): String {
+fun main(){
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val temperature = "50f"
+    val unit = temperature.takeLast(1).toUpperCase()
+    val value = temperature.dropLast(1).toDouble()
+
+    val result = if (unit == "C") {
+        (value * 9 / 5 + 32).toInt().toString() + "F"
+    } else {
+        ((value - 32) * 5 / 9).toInt().toString() + "C"
+    }
+
+    println(result)
 }
